ac261fc19bfe897fe2866a33da34d5299dedac4c
markjx <markjx@gmail.com>
2022-01-12 20:26:31 -0500

    Lastest versions of these files

--- spreadout.sh
+++ spreadout.sh
@@ -5,12 +5,12 @@
 # They output directories are balanced by file size.
 # So, each output directory should take approximately
 # the same amount of disk space.
-#
+# 
 # Sample use cases:
 # 1. You've got a bunch of files and you need to split them across file systems
-# 2. You've got a tool that processes a directory full of files. You want to
+# 2. You've got a tool that processes a directory full of files. You want to 
 #    run multiple simultaneous copies of that tool to make your job run faster.
-#    So, split your input files across multiple directories!
+#    So, split your input files across multiple directories! 
 #
 # $1 is the source folder
 # $2...$whatever are the destation folders
@@ -23,17 +23,17 @@ shift
 i=0
 while (( $# > 0 ))
 do
-        echo $i
-        destd[$i]=$1
-        let i=i+1
-        shift
+	echo $i
+	destd[$i]=$1
+	let i=i+1
+	shift
 done
 
 echo "Source Directory       : $srcf"
 echo "Destination Directories:"
 for FN in ${destd[@]}
 do
-        echo $FN
+	echo $FN
 done
 echo "Num Dest Dirs          :" ${#destd[@]}
 echo -------------------------------------------------------
@@ -42,8 +42,9 @@ echo -------------------------------------------------------
 #find $srcf -type f | while read mvme
 ls -S $srcf/* | while read mvme
 do
-        smaldir=$( du -ks ${destd[@]} | sort -n | head -n 1 | awk '{print $2;}' )
-        #mv -vi $mvme $smaldir/
-        cp -v $mvme $smaldir/
+	smaldir=$( du -ks ${destd[@]} | sort -n | head -n 1 | awk '{print $2;}' )
+	#mv -vi $mvme $smaldir/
+	cp -v $mvme $smaldir/
 done
+
 
